bde3a97e4829f4c440ad6ddbd37394fcf8cb8513
emsw.bandara <emsw.bandara@gmail.com>
2018-01-29 18:24:46 +0530

    Adding gdpr-compliance-log-processor

---
+++ components/org.wso2.carbon/src/main/java/org/wso2/carbon/LogProcessorConstants.java
@@ -0,0 +1,13 @@
+package main.java.org.wso2.carbon;
+
+public class LogProcessorConstants {
+
+    public static final String USERNAME = "username";
+    public static final String TENANT_DOMAIN = "tenantDomain";
+    public static final String TENANT_ID = "tenantId";
+    public static final String USERSTORE_DOMAIN = "userstoreDomain";
+    public static final String DOMAIN_SEPERATOR = "/";
+    public static final String PRIMARY_USERSTORE_DOMAIN = "PRIMARY";
+
+
+}
