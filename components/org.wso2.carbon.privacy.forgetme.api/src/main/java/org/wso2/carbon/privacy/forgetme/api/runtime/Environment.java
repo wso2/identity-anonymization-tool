49b01b577a815bcc826ced667f824a39973a7418
Ruwan Abeykoon <ruwana@wso2.com>
2018-02-07 11:29:31 +0530

    Add Environment variable definition
    Added the missing modification in Interface Environment

--- components/org.wso2.carbon.privacy.forgetme.api/src/main/java/org/wso2/carbon/privacy/forgetme/api/runtime/Environment.java
+++ components/org.wso2.carbon.privacy.forgetme.api/src/main/java/org/wso2/carbon/privacy/forgetme/api/runtime/Environment.java
@@ -1,6 +1,18 @@
 package org.wso2.carbon.privacy.forgetme.api.runtime;
 
+import java.util.Map;
+
+/**
+ * Definition for runtime environment variables set.
+ */
 public interface Environment {
 
     String getProperty(String name);
+
+    /**
+     * Returns a map view of the Environment variables.
+     *
+     * @return
+     */
+    Map<String, String> asMap();
 }
